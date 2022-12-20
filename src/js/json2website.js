f08936ee7f607a47057be49db6debf11bf54967e
ajh657 <aahoo01@gmail.com>
2022-12-20 17:58:41 +0200

    initial build

---
+++ src/js/json2website.js
@@ -0,0 +1,22 @@
+async function json2website(url) {
+    var absoluteURL = new URL(url, document.baseURI).href
+
+    var responce = await fetch(absoluteURL)
+    var json = await responce.text()
+
+    var parsedData = JSON.parse(json)
+    console.log(parsedData)
+
+    Object.keys(parsedData).forEach(function (key) {
+        if (key == "links") {
+            var linkDiv = document.getElementById("links")
+            parsedData[key].forEach(function (item) {
+                var newLink = document.createElement("div")
+                newLink.innerHTML = `<a href="${item.link}" class="link d-flex p-2 m-2 rounded" style="background:  #${item.linkColor};"><i class="${item.icon}" style="color:  #${item.iconColor};"></i><span class="text-center">${item.linkText}</span><span class="link-spacer"></span><span>${item.linkDescription}</span></a>`
+                linkDiv.appendChild(newLink)
+            })
+        } else {
+            document.getElementById(key).innerText = parsedData[key]
+        }
+    })
+}
