471794f4ff944f9424e89330ccb1db52e3612102
ajh657 <aahoo01@gmail.com>
2023-12-24 17:24:12 +0200

    Path fixes and added error handing to storyloader

--- Furry/FFXIV/src/js/storymanager.js
+++ Furry/FFXIV/src/js/storymanager.js
@@ -1,20 +1,22 @@
-async function getStory() {
+async function GetStory() {
 	const urlParams = new URLSearchParams(window.location.search);
 	const story = urlParams.get("story");
 
 	if (story !== null) {
 		var storyAddress = "./HTML/" + story + ".html";
-        var storyhtml =  await fetch(storyAddress)
+		var storyhtml = await fetch(storyAddress);
 
-        if (storyhtml.ok) {
-            var storytext = await storyhtml.text()
-            document.getElementById("story-container").innerHTML = storytext;
-        
-            document.title = document.querySelector("#title-block-header > .title").innerText;
-        }
+		if (storyhtml.ok) {
+			var storytext = await storyhtml.text();
+			document.getElementById("story-container").innerHTML = storytext;
+
+			document.title = document.querySelector("#title-block-header > .title").innerText;
+		} else {
+			document.getElementById("story-container").innerHTML = "<h1>404</h1><p>Story not found.</p>";
+		}
+	} else {
+		document.getElementById("story-container").innerHTML = "<h1>404</h1><p>Story not found.</p>";
 	}
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    getStory().then(() => {});
-});
+window.addEventListener("DOMContentLoaded", () => GetStory(), false);
