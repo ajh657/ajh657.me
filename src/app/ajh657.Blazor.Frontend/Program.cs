6222631cf06fa3b9f2fec6af035a7ce8e35261d7
ajh657 <aahoo01@gmail.com>
2024-01-25 20:35:56 +0200

    Initial setup + rename

---
+++ src/app/ajh657.Blazor.Frontend/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Radzen;
+
+namespace ajh657.Blazor.Frontend
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            var services = builder.Services;
+            var rootComponent = builder.RootComponents;
+
+            rootComponent.Add<App>("#app");
+            rootComponent.Add<HeadOutlet>("head::after");
+
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            services.AddRadzenComponents();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
