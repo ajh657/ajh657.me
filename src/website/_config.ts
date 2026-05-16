ca33ef35fd0fd82dbe02d26772afd90d736da2f1
ajh657 <aahoo01@gmail.com>
2026-05-16 04:50:27 +0300

    Proper start to Website Rewrite

---
+++ src/website/_config.ts
@@ -0,0 +1,12 @@
+import lume from "lume/mod.ts";
+import nav from "lume/plugins/nav.ts";
+
+const site = lume({ src: "./src", prettyUrls: false });
+
+site.use(nav());
+
+site.add("_well-known/fursona", ".well-known/fursona");
+site.add("img", "src/img");
+site.add("css", "src/css");
+
+export default site;
