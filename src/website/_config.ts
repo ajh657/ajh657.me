b00045b7dcd8798a33758b26fa64706dba5af28c
Arttu Höylä <aahoo01@gmail.com>
2026-07-29 21:40:40 +0300

    well known fix

--- src/website/_config.ts
+++ src/website/_config.ts
@@ -5,7 +5,7 @@ const site = lume({ src: "./src", prettyUrls: false });
 
 site.use(nav());
 
-site.add("_well-known", ".well-known");
+site.add("_well-known", "_well-known");
 site.add("img", "src/img");
 site.add("css", "src/css");
 
